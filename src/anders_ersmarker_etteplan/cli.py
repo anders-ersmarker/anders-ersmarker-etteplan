1b75ceb2004dd9dde1ec942edd4280c0d432823b
anders-ersmarker <100442905+anders-ersmarker@users.noreply.github.com>
2026-05-13 15:56:26 +0200

    Parse xml and find translation^

--- src/anders_ersmarker_etteplan/cli.py
+++ src/anders_ersmarker_etteplan/cli.py
@@ -1,8 +1,18 @@
 # Import packages
+import xml.etree.ElementTree as ET # Standard library for XML
+from pathlib import Path # Standard library for file paths
 
-# Parse XML from file
-# Extract text from XML
-# Save to file
 
 def main():
-    print("Hello, World!")
+
+    # Parse XML from file
+    current_dir = Path(__file__).parent
+    tree = ET.parse(current_dir / "data" / "gentext.xml")
+    root = tree.getroot()
+
+    # Extract text from XML
+    target_element = root.findall(f".//trans-unit[@id='42016']/target")
+    translated_text = target_element[0].text
+
+    # Save to file
+    print(f"<trans-unit> with id 42016 has <target> value: {translated_text}")
